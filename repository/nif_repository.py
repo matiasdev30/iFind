3e90de4d3db5e88c3b36dc0f1e8e76c82d5719a3
matiasdev30 <matiasdev30@gmail.com>
2023-01-03 16:07:20 +0100

    change response bosy

--- repository/nif_repository.py
+++ repository/nif_repository.py
@@ -1,12 +1,18 @@
 from db import get_value, set_value
 from repository import get_nif_datasource
 
+
 def get_nif_repository(nif: str) -> dict:
     result = get_value(nif=nif)
     if result != None:
-        return { "origin": "iFind", "nome": result}
+        return {"origin": "iFind", "data": {
+            "nif": nif,
+            "name": name
+        }}
     else:
         name = get_nif_datasource(nif=nif)
         set_value(key=nif, value=name)
-        return { "origin": "remote", "nome": name}
-
+        return {"origin": "remote", "data": {
+            "nif": nif,
+            "name": name
+        }}
